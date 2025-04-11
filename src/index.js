187f879f076a8f4287a47443fc867184266bd587
JoelynChua <joelynschool@gmail.com>
2025-04-11 14:22:54 +0800

    Update vercel.json

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ import About from './components/about.jsx';
 import Portfolio from './components/portfolio.jsx';
 import Contact from './components/contact.jsx';
 import BackToTop from './components/back-top.jsx';
-// import Preloader from './components/preloader';
+import Preloader from './components/preloader';
 
 
 
@@ -38,7 +38,7 @@ ReactDOM.render(
         <Portfolio />
         <Contact />
         <BackToTop />
-        {/* <Preloader /> */}
+        <Preloader />
     </React.Fragment>,
 document.getElementById('root'));
 
