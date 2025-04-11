4ead71c45d82b4770bef6aabc86f72e29f3ad264
JoelynChua <joelynschool@gmail.com>
2025-04-11 14:37:07 +0800

    Update to use react-simple-typewriter

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./stars.scss";
-import Typed from "react-typed";
+import { Typewriter } from "react-simple-typewriter";
 
 class Intro extends React.Component {
   render() {
@@ -16,18 +16,21 @@ class Intro extends React.Component {
             <div className="container text-center">
               <h1 className="intro-title mb-4">Hello, I am Joelyn Chua</h1>
               <p className="intro-subtitle">
-                <span className="text-slider-items" aria-hidden="true"></span>
                 <strong className="text-slider">
-                  <Typed
-                    strings={[
-                      "Full Stack Developer",
-                      "UI/UX Designer"
-                    ]}
-                    typeSpeed={80}
-                    backDelay={1100}
-                    backSpeed={30}
-                    loop
-                  />
+                  <span>
+                    <Typewriter
+                      words={[
+                        "Full Stack Developer",
+                        "UI/UX Designer"
+                      ]}
+                      loop
+                      cursor
+                      cursorStyle="|"
+                      typeSpeed={80}
+                      deleteSpeed={40}
+                      delaySpeed={1500}
+                    />
+                  </span>
                 </strong>
               </p>
               <p className="pt-3">
