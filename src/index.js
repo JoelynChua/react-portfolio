06fe98139292e82becc214042c94a3c7afbc9872
JoelynChua <joelynschool@gmail.com>
2025-04-11 13:24:34 +0800

    Updated vercel.json

--- src/index.js
+++ src/index.js
@@ -20,29 +20,27 @@ import 'lightbox2/dist/js/lightbox.min.js';
 import * as serviceWorker from './serviceWorker';
 
 //import components
-// import Navbar from './components/navbar.jsx';
-// import Intro from './components/intro.jsx';
-// import About from './components/about.jsx';
-// import Portfolio from './components/portfolio.jsx';
-// import Contact from './components/contact.jsx';
-// import BackToTop from './components/back-top.jsx';
-// import Preloader from './components/preloader';
+import Navbar from './components/navbar.jsx';
+import Intro from './components/intro.jsx';
+import About from './components/about.jsx';
+import Portfolio from './components/portfolio.jsx';
+import Contact from './components/contact.jsx';
+import BackToTop from './components/back-top.jsx';
+import Preloader from './components/preloader';
 
 
 
 ReactDOM.render(
-    <h1 style={{ color: 'black', textAlign: 'center', marginTop: '100px' }}> Hello from Joelyn’s Portfolio</h1>,
-  document.getElementById('root'));
-//     <React.Fragment>
-//         <Navbar />
-//         <Intro />
-//         <About />
-//         <Portfolio />
-//         <Contact />
-//         <BackToTop />
-//         <Preloader />
-//     </React.Fragment>,
-// document.getElementById('root'));
+    <React.Fragment>
+        <Navbar />
+        <Intro />
+        <About />
+        <Portfolio />
+        <Contact />
+        <BackToTop />
+        <Preloader />
+    </React.Fragment>,
+document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
